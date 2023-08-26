47167c9afb587042ca1afd685c2a933b17a9b308
daonguyen2019 <ngxuandao2015@gmail.com>
2023-08-26 14:06:57 +0700

    'committed'

--- AdvancedSQLCourse.sql
+++ AdvancedSQLCourse.sql
@@ -35,47 +35,6 @@ If Order by is specified, then the frame is RANGE BETWEEN UNBOUNDED PRECEDING AN
 If Order by is NOT specified, then the frame is ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING
 
  
-LAG(expression, offset, [,default]) OVER(order_by_clause)
--->expression: this can be a field, it can also contain a built-in function but not another analytical function
--->offset: optional. This is the offset from the current row
--->default: default is the value to be returned if offset goes beyond the scope of the partition. It default to NULL
--->order by clause: LAG() must be used with an aorder by clause in the OVER clause in SQL
---> LAG return the value in previous rows
---> Example:
-	lag(name) over (order by name)
-	lag(name, 2) over (order by name)
-	lag(name,2,'blank') over (order by name)
-
-LEAD function is opposite with LAG
-NTILE(number) OVER (order by clause)
---> example: 
-		-->ntitle(3) over (order by population)
-		-->ntitle(3) over (partition by region_id order by population)
-NTH_VALUE(expression,n) OVER()
--->example:
-		-->nth_value(name,2) over()
-		-->nth_value(name,2) over(partition by region_id)
-RANKING function
-row_number() over (order by population) --> assign 1 and incremental for each row
-rank() over (order by population) --> rank will skip the numbers if we have same numbers in ranking
-dense_rank() over (order by population) --> rank will not skip the numbers if we have same numbers in ranking
-
-DISTRIBUTION function
-percent_rank() over (order_by_clause) --> (rank-1)/(total number of rows - 1)
-
-cum_dist() over (order_by_clause) --> (num of rows <= current rows value/(total rows in partiton)
-
-ORDER OF EXECUTION
-	FROM
-	JOIN
-	WHERE
-	GROUP BY
-	HAVING 
-	WINDOW
-	SELECT
-	ORDER BY
-	LIMIT/FETCH/TOP
-
 
 
 
--- Scikit-Learn.py
+++ Scikit-Learn.py
@@ -13,6 +13,8 @@ predictions = mymodel.predict(X_test)
 from sklearn.metrics import error_metric
 performance = error_metric(y_test,predictions)
  
+ 
+
 #Evaluation metric for Regression
     #Mean Absolute Error (MAE)
         #average of absolute diff between predicted values & actual values  
@@ -35,6 +37,89 @@ from sklearn.linear_model import LinearRegression
 model = LinearRegression()
 model.fit(X_train, y_train)
 model.predict(X_test)
+model.coef_ #return coefficient
+
+from sklearn.metrics import mean_absolute_error,mean_squared_error
+MAE = mean_absolute_error(y_test, test_predictions)
+MSE = mean_squared_error(y_test, test_predictions)
+RMSE = np.sqrt(MSE)
+
+
+
+
+from joblib import dump, load
+dump(model,'model.joblib')
+loaded_model = load('model.joblib')
+
+campaign = [[149,22,12]]
+loaded_model.predict(campaign) #return predicted values for y
+
+from sklearn.preprocessing import PolynomialFeatures
+polynomial_converter = PolynomialFeatures(
+    degree = 2
+    , include_bias = 
+    ,
+)
+polynomial_converter.transform(X) #create interaction ....
+polynomial_converter.fit_transform(X)
+
+
+#########Bias - variance Trade off
+#Overfitting versus Underfitting
+underfit = high bias, low variance, simple model 
+overfit = high variance , low error on training sets but high error on test/validation sets
+
+--> To select an ideal complexity we have to rely on the train error and test error also
+
+###Choose degree of Polynomial ####
+# Create the different order poly
+# Split poly feat train/test
+# Fit on train
+# Store/save the RMSE for both the TRAIN and TEST
+# Plot the results (error vs poly order)
+
+train_rmse_errors = []
+test_rmse_errors = []
+
+
+for d in range(1,10):
+    poly_converter = PolynomialFeatures(degree=d, include_bias = False)
+    poly_features = poly_converter.fit_transform(X)
+    X_train, X_test, y_train, y_test = train_test_split(poly_features,y, test_size = , random_state = )
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
